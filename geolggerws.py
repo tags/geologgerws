32b6f3611c0b2d60e383eddf835a5d41244dca8f
Jonah Duckles <jonahd@gmail.com>
2013-07-10 11:17:16 -0500

    added license

--- geolggerws.py
+++ geolggerws.py
@@ -1,3 +1,33 @@
+################################################################################
+# The MIT License (MIT)
+#
+# Copyright (c) 2013 The University of Oklahoma
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.                                   
+################################################################################
+#
+# A web service to get data stored in mongodb for the geologger web application.
+# 
+# Jonah Duckles - jduckles@ou.edu
+# 
+
+
 import cherrypy
 import pymongo
 import psycopg2
@@ -43,7 +73,7 @@ class Root(object):
             else:
                 return "{error: Nothing found with that tagname}"
         else:
-            tags = [ item for item in col.find(spec={useridkey: username}, fields={tagnamekey:True, '_id':False}) ]
+            tags = [ item for item in col.distinct(spec={useridkey: username}, fields={tagnamekey:True, '_id':False}) ]
             return json.dumps(tags, default=handler, indent=2)
 
     @cherrypy.expose
